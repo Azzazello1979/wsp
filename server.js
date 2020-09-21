e2bf887f51a84248b8e9c2587c3f7df34ea4e4a5
Balint H <balint.haui@gmail.com>
2020-09-21 18:34:20 +0200

    Express serving images

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+console.log(__dirname);
+app.use("/public", express.static(path.join(__dirname + "/public")));
 
 // require routeControllers
 const usersRoute = require("./server-core/routeControllers/usersController");
