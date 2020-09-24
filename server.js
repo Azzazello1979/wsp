085210d553be1d56ae7e27a3ae0c1bba9e96c12e
Balint H <balint.haui@gmail.com>
2020-09-24 10:34:31 +0200

    using multer to save images on server

--- server.js
+++ server.js
@@ -3,17 +3,27 @@
 // module imports
 const express = require("express");
 const app = express();
-require("dotenv").config(); // process .env files
+require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 const multer = require("multer");
 const PORT = process.env.PORT;
 
+// config multer
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "rawimages");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
 // use middlewares...
 app.use(cors());
-app.use(multer({ dest: "rawimages" }).single("image"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(multer({ storage: multerStorage }).single("image"));
 app.use("/public", express.static(path.join(__dirname + "/public")));
 
 // require routeControllers
